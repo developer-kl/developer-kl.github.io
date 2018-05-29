630a29455fa8e7fedb097730f31c2cf1bdaa7769
abhijith <developer-kl@users.noreply.github.com>
2018-05-29 19:29:49 +0530

    navbar fixed - seventh

--- js/custom.js
+++ js/custom.js
@@ -108,6 +108,26 @@
 				$(".modal").prependTo( "body" );
 			});
 		}
+/*
+$(document).click(function (event) {
+    
+        $(".navbar-collapse").collapse();
+//        $(".navbar-collapse").collapse('hide');
 
+//$('.collapse').collapse()
+});
+$(document).click(function (event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");               
+    var _opened = $navbar.hasClass("in");
+//    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+        $navbar.collapse('hide');
+ //   }
+});
+*/
+$('.navbar-collapse ul li a').click(function(){
+$('.navbar-toggle:visible').click();
+});
 	}); // End document ready
 })(this.jQuery);
+
